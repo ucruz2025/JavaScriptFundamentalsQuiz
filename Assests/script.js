16e01d07065bb041cd3875805f09b0f6cdff459c
Uriel Cruz <ucruz2025@gmail.com>
2021-02-17 15:59:55 -0500

    Added basic HTML, CSS, and JavaScript to Repository.

---
+++ Assests/script.js
@@ -0,0 +1,26 @@
+var startQuiz = document.querySelector("#startQuiz")//Need to add button to HTML
+
+//Need to add eventListener for start button
+startQuiz.addEventListener("click", function(){
+    //Need to include function to start quiz
+});
+
+//Look how to use timeInterval to set time countdown
+function quizTimer(){
+    var timeLeft = 0;
+
+    //if(timeLeft )
+}
+
+//function that presents the series of questions?
+
+//Preventing Default
+
+//Boolean function that determines if button pressed is correct or incorrect
+function checkAnswer(){
+
+}
+
+//If all questions are answered before, then timer reaches 0 and game is over
+
+//Save initals to the local storage?
