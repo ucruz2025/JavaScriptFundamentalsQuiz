cd46ca3d6ffd0222228a9c7c567977ff4970a50b
Uriel Cruz <ucruz2025@gmail.com>
2021-02-25 11:11:56 -0500

    Finished editing JavaScript files

--- Assests/JavaScripts/highscores.js
+++ Assests/JavaScripts/highscores.js
@@ -1,4 +1,23 @@
-//eventListener to view high scores
-//viewHiSc.addEventListener("click", function(){
-//
-//});
+var disHighscores = document.querySelector('#highscores');
+var clearHighscores = document.querySelector('#clear');
+
+function viewHighscores(){
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    for(var i = 0; i < highscores.length; i++){
+        var highscore = highscores[i];
+
+        var li = document.createElement('li');
+        li.setAttribute('data-index', i)
+        li.textContent = highscore.initials + " - " + highscore.score;
+        
+        disHighscores.appendChild(li);
+    }
+}
+
+clearHighscores.addEventListener("click", function(){
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+});
+
+viewHighscores();
--- Assests/JavaScripts/script.js
+++ Assests/JavaScripts/script.js
@@ -4,10 +4,12 @@ var time = questions.length * 10;
 var timerId;
 
 var timeLeft = document.querySelector('#time');
-var startButton = document.querySelector('#startQuiz');//Need to add button to HTML
+var startButton = document.querySelector('#startQuiz');
 var questionsSec = document.querySelector('#questions');
 var choicesSec = document.querySelector('#choices');
 var feedbackSec =  document.querySelector('#feedback');
+var userInitials = document.querySelector('#userInitials');
+var submitButton = document.querySelector('#submit');
 
 
 //Starts quiz once button clicked
@@ -33,6 +35,10 @@ function quizTimer(){
     time--;
     timeLeft.textContent = time; 
 
+    if (time < 0){
+        time = 0;
+    }
+
     if(time <= 0){
         endQuiz();
     }
@@ -49,18 +55,22 @@ function displayQuestion(){
     choicesSec.setAttribute('class', 'choices');
 
     var firstOpt = document.getElementById('a');
+    firstOpt.setAttribute('value', choicesDis[0])
     firstOpt.textContent = choicesDis[0];
     firstOpt.onclick = checkAnswer;
 
     var secondOpt = document.getElementById('b');
+    secondOpt.setAttribute('value', choicesDis[1])
     secondOpt.textContent = choicesDis[1];
     secondOpt.onclick = checkAnswer;
 
     var thirdOpt = document.getElementById('c');
+    thirdOpt.setAttribute('value', choicesDis[2])
     thirdOpt.textContent = choicesDis[2];
     thirdOpt.onclick = checkAnswer;
     
     var fourthOpt = document.getElementById('d');
+    fourthOpt.setAttribute('value', choicesDis[3])
     fourthOpt.textContent = choicesDis[3];
     fourthOpt.onclick = checkAnswer;
 }
@@ -71,7 +81,6 @@ function checkAnswer(){
         feedbackSec.textContent = 'Correct!';
     } else {
         time -= 5;
-
         timeLeft.textContent = time;
         feedbackSec.textContent = 'Ooo, Try again';
     }
@@ -96,11 +105,29 @@ function endQuiz(){
     var endQuiz = document.getElementById('end');
     endQuiz.setAttribute('class', 'on');
 
+    var finalScore = document.getElementById('finalScore');
+    finalScore.textContent = time;
+
     questionsSec.setAttribute('class', 'off');
 }
 
-function saveHighscores(){
+submitButton.addEventListener("click", function(){
+    var initials = userInitials.value.trim();
 
-}
+    if(initials == ""){
+        alert('Make sure to put in your initials.');
+        return;
+    } else{
+        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-//Save initals to the local storage?
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.location.href = 'highscores.html';
+    }
+})
